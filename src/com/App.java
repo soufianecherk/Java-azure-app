2fe819e499f4be683b84fd87680e5c5133ca2418
Soufiane <soufiane.cherkaoui01@gmail.com>
2025-04-11 17:14:51 +0100

    Quick fix

--- src/com/App.java
+++ src/com/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java inside Docker!");
+        System.out.println("Hello from Java App!");
     }
 }
